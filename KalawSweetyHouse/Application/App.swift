5bfb8fb9520ac1d0227bfc909d508adbca7a3275
Ku Ku <khukhu113@gmail.com>
2023-11-28 21:27:38 +0630

    commit -m "setup folder structure & app delegate setup"

---
+++ KalawSweetyHouse/Application/App.swift
@@ -0,0 +1,60 @@
+//
+//  App.swift
+//  KalawSweetyHouse
+//
+//  Created by Ku Ku Zan on 11/28/23.
+//
+
+import Foundation
+import UIKit
+
+final class App: NSObject {
+    
+    /// Shared singleton app instance
+    static let shared = App()
+    
+    /// Window associated with the app.
+    var window: UIWindow!
+    
+    /// The window scene of this app.
+    var windowScene: UIWindowScene! {
+        UIApplication.shared.connectedScenes.first {
+            $0 is UIWindowScene
+        } as? UIWindowScene
+    }
+}
+
+
+extension App {
+    
+    /// Prepare UI for fresh launches.
+    /// Call this method on app launch or scene init
+    func prepareForUI(using window: UIWindow) {
+        // retain window
+        self.window = window
+        window.backgroundColor = .white
+        
+        renderDashboard()
+    }
+}
+
+extension App {
+    /// Render
+    func renderDashboard() {
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .yellow
+        setRoot(viewController: viewController)
+    }
+}
+
+extension App {
+    func setRoot(viewController: UIViewController, animated: Bool = true) {
+        window.rootViewController = viewController
+        UIView.transition(
+            with: window,
+            duration: 0.25,
+            options: .transitionCrossDissolve,
+            animations: nil
+        )
+    }
+}
---
+++ KalawSweetyHouse/Application/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  KalawSweetyHouse
+//
+//  Created by Ku Ku Zan on 11/28/23.
+//
+
+import UIKit
+
+UIApplicationMain(
+    CommandLine.argc, CommandLine.unsafeArgv,
+    NSStringFromClass(UIApplication.self),
+    NSStringFromClass(AppDelegate.self)
+)
